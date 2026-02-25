0ae0e3387135cb86f3d703fa53f77bf721f3a2f7
C0BecTb11 <xaebas120@gmail.com>
2026-02-25 01:51:07 +0100

    Update unit.js

--- unit.js
+++ unit.js
@@ -1,13 +1,35 @@
 const UNIT_TYPES = {
-    SOLDIER: { id: 'soldier', name: "Пехота", imgSrc: "./images/team-r-soldier.png", cost: 50, maxHp: 10, attack: 2, attackRange: 1, moveRange: 2, canCapture: true },
-    RPK: { id: 'rpk', name: "Пулемет", imgSrc: "./images/team-r-rpk.png", cost: 80, maxHp: 10, attack: 4, attackRange: 2, moveRange: 1, canCapture: true },
-    RPG: { id: 'rpg', name: "Гранатометчик", imgSrc: "./images/team-r-rpg.png", cost: 100, maxHp: 8, attack: 7, attackRange: 2, moveRange: 2, canCapture: true },
-    SUPPLY: { id: 'supply', name: "Снабжение", imgSrc: "./images/team-r-supply.png", cost: 120, maxHp: 12, attack: 0, attackRange: 0, moveRange: 5, canCapture: false },
-    BTR: { id: 'btr', name: "БТР", imgSrc: "./images/team-r-btr.png", cost: 250, maxHp: 25, attack: 5, attackRange: 3, moveRange: 4, canCapture: false },
-    TANK: { id: 'tank', name: "Танк", imgSrc: "./images/team-r-tank.png", cost: 500, maxHp: 50, attack: 12, attackRange: 4, moveRange: 3, canCapture: false }
+    SOLDIER: { 
+        id: 'soldier', name: "Пехота", imgSrc: "./images/team-r-soldier.png", 
+        cost: 50, maxHp: 10, attack: 2, attackRange: 1, moveRange: 2, canCapture: true 
+    },
+    RPK: { 
+        id: 'rpk', name: "Пулемет", imgSrc: "./images/team-r-rpk.png", 
+        // Бафф: было 10 хп и 4 урона. Стало 15 хп и 5 урона. Гроза пехоты!
+        cost: 80, maxHp: 15, attack: 5, attackRange: 2, moveRange: 1, canCapture: true 
+    },
+    RPG: { 
+        id: 'rpg', name: "Гранатометчик", imgSrc: "./images/team-r-rpg.png", 
+        // Нерф базы, бафф против брони: базовый урон всего 2. Но по технике добавит еще 13 (итого 15!)
+        cost: 100, maxHp: 8, attack: 2, attackRange: 2, moveRange: 2, canCapture: true, 
+        bonusArmorDamage: 13 
+    },
+    SUPPLY: { 
+        id: 'supply', name: "Снабжение", imgSrc: "./images/team-r-supply.png", 
+        cost: 120, maxHp: 12, attack: 0, attackRange: 0, moveRange: 5, canCapture: false 
+    },
+    BTR: { 
+        id: 'btr', name: "БТР", imgSrc: "./images/team-r-btr.png", 
+        cost: 250, maxHp: 25, attack: 5, attackRange: 3, moveRange: 4, canCapture: false, 
+        isArmor: true // <--- Уязвимость к РПГ
+    },
+    TANK: { 
+        id: 'tank', name: "Танк", imgSrc: "./images/team-r-tank.png", 
+        cost: 500, maxHp: 50, attack: 12, attackRange: 4, moveRange: 3, canCapture: false, 
+        isArmor: true // <--- Уязвимость к РПГ
+    }
 };
 
-// Глобальный объект для хранения загруженных картинок
 const loadedImages = {};
 
 function preloadUnitImages(callback) {
@@ -19,8 +41,7 @@ function preloadUnitImages(callback) {
         const img = new Image();
         img.src = UNIT_TYPES[key].imgSrc;
         img.onload = () => {
-            // ИСПРАВЛЕНО: Сохраняем картинку по точному id ('soldier', 'rpk' и т.д.)
-            loadedImages[UNIT_TYPES[key].id] = img; 
+            loadedImages[UNIT_TYPES[key].id] = img;
             loadedCount++;
             if (loadedCount === keys.length) callback();
         };
