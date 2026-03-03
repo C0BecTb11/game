baad051797480f8ffd64c1e9e41cfaa7cb2e3734
C0BecTb11 <xaebas120@gmail.com>
2026-03-03 03:08:16 +0100

    Update unit.js

--- unit.js
+++ unit.js
@@ -53,7 +53,39 @@ const UNIT_TYPES = {
         id: 'tank', name: "Танк", imgSrc: "./images/team-r-tank.png", 
         cost: 350, maxHp: 50, attack: 12, attackRange: 3, moveRange: 4, visionRange: 5, canCapture: false, 
         isArmor: true 
-    }
+    },
+        RSZO: { 
+        id: 'rszo', name: "РСЗО", imgSrc: "./images/team-r-rszo.png", 
+        cost: 400, maxHp: 20, attack: 30, attackRange: 15, moveRange: 3, visionRange: 6, canCapture: false, 
+        isArmor: true, isArtillery: true, artArea: 4, artShots: 3, maxCooldown: 4 
+    },
+    MORTAR: { 
+        id: 'mortar', name: "Миномёт", imgSrc: "./images/team-r-minomet.png", 
+        cost: 150, maxHp: 10, attack: 20, attackRange: 8, moveRange: 2, visionRange: 5, canCapture: true,
+        isInfantry: true, isArtillery: true, artArea: 2, artShots: 1, maxCooldown: 0 
+    },
+    PZRK: { // Противовоздушная пехота
+        id: 'pzrk', name: "Солдат ПЗРК", imgSrc: "./images/team-r-ptrk.png", 
+        cost: 120, maxHp: 10, attack: 25, attackRange: 5, moveRange: 2, visionRange: 6, canCapture: true,
+        isInfantry: true, isAntiAir: true // Специальный флаг для ПВО
+    },
+
+    // === ВОЗДУШНАЯ ТЕХНИКА ===
+    MI8: { 
+        id: 'mi8', name: "Ми-8АМТШ", imgSrc: "./images/team-r-mi8.png", 
+        cost: 250, maxHp: 25, attack: 5, attackRange: 2, moveRange: 8, visionRange: 7, canCapture: false, 
+        isAir: true, transportCapacity: 4 
+    },
+    KA52: { 
+        id: 'ka52', name: "Ка-52 Аллигатор", imgSrc: "./images/team-r-ka52.png", 
+        cost: 450, maxHp: 35, attack: 15, attackRange: 4, moveRange: 7, visionRange: 7, canCapture: false, 
+        isAir: true, bonusArmorDamage: 15 
+    },
+    SU25: { 
+        id: 'su25', name: "Су-25СМ3", imgSrc: "./images/team-r-su25.png", 
+        cost: 600, maxHp: 40, attack: 25, attackRange: 5, moveRange: 10, visionRange: 8, canCapture: false, 
+        isAir: true, bonusArmorDamage: 20 
+}
 };
 
 const loadedImages = {};
